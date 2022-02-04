a1362d6a50102641117780274b62f7f8c9d48021
dmalex <dmaleex-m2@yandex.ru>
2022-02-04 14:26:42 +0300

    Initial Commit

---
+++ SteamGames/SteamGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteamGamesApp.swift
+//  SteamGames
+//
+//  Created by brfsu on 04.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SteamGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
