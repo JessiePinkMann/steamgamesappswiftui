c5c08651a5fec14b44e8477eed8311af0ec1899f
Egor Zheliba <egor.anoshin.03@mail.ru>
2024-01-30 18:57:55 +0300

    steam API sample

---
+++ SteamGames/Api.swift
@@ -0,0 +1,8 @@
+//
+//  Api.swift
+//  SteamGames
+//
+//  Created by Egor Anoshin on 23.01.2024.
+//
+
+import Foundation
---
+++ SteamGames/DetailView.swift
@@ -0,0 +1,8 @@
+//
+//  DetailView.swift
+//  SteamGames
+//
+//  Created by Egor Anoshin on 23.01.2024.
+//
+
+import Foundation
